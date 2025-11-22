dea12e1f1381608137b39498cca19754bfd0d09a
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2025-11-22 23:59:26 +0500

    Update deepCopy.js

--- deepCopy.js
+++ deepCopy.js
@@ -1,3 +1,4 @@
 // Deep Copy
 // A deep copy creates a completely independent copy of the object, including all nested objects or arrays. This ensures that changes  
 // made to one object do not affect the other, as each object, along with its nested structures, resides in a separate memory location. 
+const obj1 = {}
