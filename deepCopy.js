d5274ae4706f0e7034a1aed23f7f4244562de8b4
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2025-11-23 00:02:31 +0500

    Update deepCopy.js

--- deepCopy.js
+++ deepCopy.js
@@ -1,4 +1,7 @@
 // Deep Copy
 // A deep copy creates a completely independent copy of the object, including all nested objects or arrays. This ensures that changes  
 // made to one object do not affect the other, as each object, along with its nested structures, resides in a separate memory location. 
-const obj1 = {}
+const obj1 = {a:1,b:2,c: ['a',',b','c']};
+const obj2 = JSON.stringify(obj1);
+// now if we can change anhthing in obj2 will not  
+//  affect the obj1
