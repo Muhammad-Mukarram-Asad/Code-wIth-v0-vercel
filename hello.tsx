23c8a58a464fc589948f748870d32b96efe6c15e
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2025-12-30 23:59:19 +0500

    Add hello.tsx component with initial imports

---
+++ hello.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React, { useEffect, useRef, useState } from 'react';
+import styles from './getCategorizing.module.scss';
+import boyImage from '../../../public/icons/uncategorizeTrxImage.svg';
+import congratzTick from '../../../public/icons/uncategorizeCongratzTick.svg';
+import Image from 'next/image';
+import { AccountDetails } from '@/ts/interfaces/accounts.interface';
+import { CategoryDetails } from '@/ts/interfaces/category.interface';
+import { VoucherDetails } from '@/ts/interfaces/vouchers.interface';
+import { store } from '@/store/store';
+import { EditVoucher } from '@/store/voucherSlice';
+import { toast } from 'react-toastify';
+import { showToast } from '@/utils/toasts/showToast';
+import { ToastType } from '@/utils/enums/ToastTypes.enum';
+import { useRouter } from 'next/router';
